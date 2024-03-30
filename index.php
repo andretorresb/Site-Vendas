a2e19ed3e73129a149682d341dc60b85dacc00d3
andretorresb <andret44@icloud.com>
2024-03-30 17:18:46 -0400

    Criação do Cabeçalho
    Foi criado o Cabelho com ícones, nome e menu, onde o mesmo foram estilizado com CSS.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE hmtl>
+<html lang="pt-br">
+	<head>   
+		<title>Meu Portfolio</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="estilo-index.css">
+	</head>
+	<body> 
+		<header class="cabecalho">
+			<img src="imagens/icone-github.png">
+			<h4>André Torres da Cruz</h4>
+			<nav class="menu">
+				<button>Sobre Mim</button>
+				<button>Certificados</button>
+				<button>Habilidades</button>
+			</nav>
+		</header>
+	</body>
+</html>
